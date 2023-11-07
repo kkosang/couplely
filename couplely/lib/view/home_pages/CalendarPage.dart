1983dd469e34adb2d8dcb8801f224accb48a3a30
조형석 <chs9902@naver.com>
2023-11-07 19:41:16 +0900

    feat: 플로팅 액션 버튼 구현

--- couplely/lib/view/home_pages/CalendarPage.dart
+++ couplely/lib/view/home_pages/CalendarPage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:couplely/view/home_pages/BottomSheet.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -10,12 +12,52 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  bool floatingEnable = true;
+  DateTime focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_circle),
-        onPressed: () {},
+      floatingActionButton: Stack(
+        children: <Widget>[
+          floatingEnable
+              ? Align(
+                  alignment: Alignment(
+                      Alignment.bottomRight.x, Alignment.bottomRight.y - 0.4),
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    tooltip: '일정',
+                    child: Icon(Icons.fact_check_outlined),
+                  ))
+              : Container(),
+          floatingEnable
+              ? Align(
+                  alignment: Alignment(
+                      Alignment.bottomRight.x, Alignment.bottomRight.y - 0.2),
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    tooltip: '다이어리',
+                    child: Icon(Icons.book),
+                  ))
+              : Container(),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    floatingEnable = !floatingEnable;
+                  });
+                },
+                tooltip: '추가',
+                child: Icon(Icons.add),
+              )),
+        ],
       ),
       body: Stack(children: [
         Positioned(
@@ -51,7 +93,7 @@ class _CalendarPageState extends State<CalendarPage> {
               Container(
                 color: Colors.grey.shade800,
                 child: Container(
-                  height: MediaQuery.of(context).size.height*0.7,
+                  height: MediaQuery.of(context).size.height * 0.7,
                   decoration: BoxDecoration(
                       color: Colors.orange[50],
                       borderRadius:
@@ -61,7 +103,19 @@ class _CalendarPageState extends State<CalendarPage> {
                     child: TableCalendar(
                       firstDay: DateTime.utc(2023, 11, 1),
                       lastDay: DateTime.utc(2030, 12, 31),
-                      focusedDay: DateTime.now(),
+                      focusedDay: focusedDay,
+                      onDaySelected:
+                          (DateTime selectedDay, DateTime focusedDay) {
+                        // 선택된 날짜의 상태를 갱신합니다.
+                        setState(() {
+                          this.selectedDay = selectedDay;
+                          this.focusedDay = focusedDay;
+                        });
+                      },
+                      selectedDayPredicate: (DateTime day) {
+                        // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+                        return isSameDay(selectedDay, day);
+                      },
                       headerVisible: false,
                       calendarStyle: CalendarStyle(
                           outsideDaysVisible: false,
